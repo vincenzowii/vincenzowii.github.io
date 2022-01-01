2b860260adab973fef0d578de7e358fc272c7552
vincenzowii <93964966+vincenzowii@users.noreply.github.com>
2022-01-01 09:44:47 +0500

    Delete js directory

--- js/cart.js
+++
@@ -1,5 +0,0 @@
-$('.add__info-show').click(function () {
-    $(this).children('span').toggle();
-
-    $(this).parent().parent().siblings('.cart__item-info-add').toggleClass('cart__item-info-add--active')
-})
